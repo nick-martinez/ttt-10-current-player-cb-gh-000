10a61319c5d9b65d6818b2e94ba699c523d488c8
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-19 21:47:05 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  counter = 0
+  square = 0
+
+  while (square < board.size)
+    if (board[square] != " " && board[square] != "" && board[square] != nil)
+      counter += 1
+    end
+    square += 1
+  end
+
+  return counter
+end
+
+def current_player(board)
+  if (turn_count(board) % 2 == 0)
+    return "X"
+  else
+    return "O"
+  end
+end
