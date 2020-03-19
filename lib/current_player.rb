f9a7460511611b26d7b8111386fbfae6cc284356
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-19 21:13:29 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |sqaure|
+    counter += 1
+  end
+
+  return counter
+end
+
+def current_player(board)
+  if (counter % 2 = 0)
+    return "X"
+  else
+    return "O"
+  end
+end
